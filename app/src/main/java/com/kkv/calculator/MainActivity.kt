7e46788156c4c963dc88b0af02bbcadf4dec21cf
krishnakkv <krishnakkvverma@gmail.com>
2021-05-04 20:50:20 +0530

    User input stored in list to be processed!

--- app/src/main/java/com/kkv/calculator/MainActivity.kt
+++ app/src/main/java/com/kkv/calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kkv.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     var new:Boolean = true
     var operator: Boolean =false
+    var opList= mutableListOf<String>()
+    var numList= mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private fun clickListeners() {
 
         val result:TextView = findViewById(R.id.result)
+        val exp:TextView = findViewById(R.id.expression)
 
         val b1: Button = findViewById(R.id.one)
         val b2: Button = findViewById(R.id.two)
@@ -47,91 +51,132 @@ class MainActivity : AppCompatActivity() {
         val bAC: Button = findViewById(R.id.ac)
 
         b1.setOnClickListener {
-            result.text=outText(b1.text.toString())
+            exp.text=outText(b1.text.toString())
         }
         b2.setOnClickListener {
-            result.text=outText(b2.text.toString())
+            exp.text=outText(b2.text.toString())
         }
         b3.setOnClickListener {
-            result.text=outText(b3.text.toString())
+            exp.text=outText(b3.text.toString())
         }
         b4.setOnClickListener {
-            result.text=outText(b4.text.toString())
+            exp.text=outText(b4.text.toString())
         }
         b5.setOnClickListener {
-            result.text=outText(b5.text.toString())
+            exp.text=outText(b5.text.toString())
         }
         b6.setOnClickListener {
-            result.text=outText(b6.text.toString())
+            exp.text=outText(b6.text.toString())
         }
         b7.setOnClickListener {
-            result.text=outText(b7.text.toString())
+            exp.text=outText(b7.text.toString())
         }
         b8.setOnClickListener {
-            result.text=outText(b8.text.toString())
+            exp.text=outText(b8.text.toString())
         }
         b9.setOnClickListener {
-            result.text=outText(b9.text.toString())
+            exp.text=outText(b9.text.toString())
         }
         b0.setOnClickListener {
-            result.text=outText(b0.text.toString())
+            exp.text=outText(b0.text.toString())
         }
         bAdd.setOnClickListener {
-            result.text=operatorFun(bAdd.text.toString())
+            exp.text=operatorFun(bAdd.text.toString())
         }
         bSubtract.setOnClickListener {
-            result.text=operatorFun(bSubtract.text.toString())
+            exp.text=operatorFun(bSubtract.text.toString())
         }
         bMultiply.setOnClickListener {
-            result.text=operatorFun(bMultiply.text.toString())
+            exp.text=operatorFun(bMultiply.text.toString())
         }
         bDivide.setOnClickListener {
-            result.text=operatorFun(bDivide.text.toString())
+            exp.text=operatorFun(bDivide.text.toString())
         }
         bPercent.setOnClickListener {
-            result.text=operatorFun(bPercent.text.toString())
+            exp.text=operatorFun(bPercent.text.toString())
         }
         bAC.setOnClickListener {
             result.text="0"
+            exp.text=""
             new=true
             operator=false
+            opList.clear()
+            numList.clear()
         }
         bEquals.setOnClickListener {
             // function to execute the question var to be added in all digits and here
         }
         bClear.setOnClickListener {
-            var c1=result.text.toString()
+            var c1=exp.text.toString()
+            if (c1=="")
+                return@setOnClickListener
+            if (c1.last() in arrayOf('+','—','x','÷','%')){
+                opList.removeLast()
+                new=false
+                Log.d("Main","oplist is updated by clear button ${opList}")
+            }
+            else if (c1.last() in arrayOf('1','2','3','4','5','6','7','8','9','0')){
+                var temp:String =numList.last()
+                temp=temp.take(temp.length-1)
+                numList.removeLast()
+                if (temp != ""){
+                    numList.add(temp)
+                }
+                new=true
+
+                Log.d("Main","numList updated from clear ${numList}")
+            }
             if (c1.length == 1) {
-                result.text ="0"
+                exp.text =""
             }
             else{
-                result.text = c1.take(c1.length - 1)
+                exp.text = c1.take(c1.length - 1)
             }
             operator=false
+
         }
         bDot.setOnClickListener {
-            result.text=result.text.toString()+bDot.text.toString()
+            exp.text=exp.text.toString()+bDot.text.toString()
         }
-
     }
 
     private fun operatorFun(inputText: String): CharSequence? {
         val result:TextView = findViewById(R.id.result)
+        val exp:TextView = findViewById(R.id.expression)
+        new=true
         if (!operator){
             operator = true
-            return result.text.toString()+inputText
+            opList.add((inputText))
+            Log.d("Main","oplist is updated: ${opList}")
+            return exp.text.toString()+inputText
         }
         else{
-            return result.text.toString().take(result.text.length - 1) + inputText
+            opList.removeLast()
+            opList.add(inputText)
+            Log.d("Main","oplist else updated: ${opList}")
+            return exp.text.toString().take(exp.text.length - 1) + inputText
         }
+
     }
 
-    private fun outText(inputText: String): CharSequence? {
+    private fun outText(inputText: String): CharSequence {
         val result:TextView = findViewById(R.id.result)
-        new=false
+        val exp:TextView = findViewById(R.id.expression)
+        if (new){
+            numList.add(inputText)
+            Log.d("Main","numList updated though adding ${numList}")
+            new=false
+        }
+        else{
+            var useless:String=""
+            useless=numList.last()+inputText
+            numList.removeLast()
+            numList.add(useless)
+            Log.d("Main","numList updated though adding in else ${numList}")
+        }
         operator=false
-        val temp : String = result.text.toString()+inputText
-        if(result.text.toString().take(1) == "0"){
+        val temp : String = exp.text.toString()+inputText
+        if(exp.text.toString().take(1) == "0"){
             return temp.takeLast(temp.length -1)
         }
         return temp
